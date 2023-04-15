9f04e3b8e2645882b60a2d51647724d2c94f33e0
Dan Ult <ulturgashev112@gmail.com>
2023-04-15 17:19:13 +0300

    working without nicknames

--- server/chat/models.py
+++ server/chat/models.py
@@ -1,6 +1,39 @@
-from pydantic import BaseModel
+import json
+from enum import Enum, auto
+
+from pydantic import BaseModel, ValidationError
+
+
+class MessageType(str, Enum):
+    CONNECT = "CONNECT"
+    DISCONNECT = "DISCONNECT"
+    MESSAGE = "MESSAGE"
+    PICTURE = "PICTURE"
 
 
 class User(BaseModel):
-    user_id: int
-    username: str
+    client_id: int
+    nickname: str
+
+
+class Transport(BaseModel):
+    message_type: MessageType
+    client_id: int
+    nickname: str
+    message: str
+    picture: bytes
+
+
+if __name__ == "__main__":
+    usr = {
+        "client_id": 1,
+        # "nickname": "Dan",
+        "a": 1
+    }
+    s = json.dumps(usr)
+
+    try:
+        u = User.parse_raw(s)
+        print(u)
+    except ValidationError as err:
+        print(err)
